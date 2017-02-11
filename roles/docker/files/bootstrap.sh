723deb5f7edbee847d307040d2a59805f6d9a471
Steven Truong <djatlantic@gmail>
2017-02-11 15:06:42 -0800

    adding facts

--- roles/docker/files/bootstrap.sh
+++ roles/docker/files/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+mkdir -p /etc/ansible
+
 #allow ansible-pull without specifying a host file                              
 echo "localhost ansible_connection=local" > /etc/ansible/hosts
+
+rm -rf ~/ansible
 
 ansible-pull -U https://github.com/djatlantic/ansible-aws.git -d ~/ansible aws-docker-host-bootstrap.yml -vvv
